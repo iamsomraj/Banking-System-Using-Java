473a12e40a8a575d9e051f3daee56cacc004d2ca
Somraj Mukherjee <iamsomraj@gmail.com>
2019-12-27 17:35:42 +0530

    Finished

--- App.java
+++ App.java
@@ -1,8 +1,7 @@
 import java.util.*;
 
 class Bank {
-  static LinkedHashMap<String, Integer> nameBalanceMap = new LinkedHashMap<>();
-  static LinkedHashMap<Integer, Integer> idBalanceMap = new LinkedHashMap<>();
+  static LinkedHashMap<Integer, LinkedHashMap<String, Integer>> idBalanceMap = new LinkedHashMap<>();
 
   String SUCCESS = "\nThe request is succesful!";
   String FAIL = "\nThe request is not succesful!";
@@ -17,10 +16,36 @@ class Bank {
 
 class Services extends Bank {
 
+  Scanner sc = new Scanner(System.in);
+
+  public void updateDetails(String name, int id, int balance) {
+    LinkedHashMap<String, Integer> nameBalanceMap = new LinkedHashMap<>();
+    nameBalanceMap.put(name, balance);
+    idBalanceMap.put(id, nameBalanceMap);
+  }
+
+  public int getBalance(int id) {
+
+    int balance = 0;
+    for (Map.Entry<String, Integer> entry : idBalanceMap.get(id).entrySet()) {
+      balance = entry.getValue();
+    }
+    return balance;
+  }
+
+  public String getName(int id) {
+    String name = "";
+    for (Map.Entry<String, Integer> entry : idBalanceMap.get(id).entrySet()) {
+      name = entry.getKey();
+    }
+    return name;
+  }
+
   public void displayBalance(int id) {
 
     if (idBalanceMap.containsKey(id)) {
       System.out.println(SUCCESS);
+      System.out.println(getBalance(id));
     } else {
       System.out.println(FAIL);
     }
@@ -30,6 +55,18 @@ class Services extends Bank {
   public void withdrawCash(int id) {
     if (idBalanceMap.containsKey(id)) {
       System.out.println(SUCCESS);
+      System.out.println("\nEnter the amount you want to withdraw:");
+      int amount = sc.nextInt();
+
+      if (hasBalance(id, amount)) {
+        int avail = getBalance(id);
+        String name = getName(id);
+        int balance = avail - amount;
+        updateDetails(name, id, balance);
+        System.out.println(SUCCESS);
+      } else {
+        System.out.println(FAIL);
+      }
     } else {
       System.out.println(FAIL);
     }
@@ -38,15 +75,47 @@ class Services extends Bank {
   public void depositCash(int id) {
     if (idBalanceMap.containsKey(id)) {
       System.out.println(SUCCESS);
+      System.out.println("\nEnter the amount you want to deposit:");
+      int amount = sc.nextInt();
+      int avail = getBalance(id);
+      String name = getName(id);
+      int balance = avail + amount;
+      updateDetails(name, id, balance);
+      System.out.println(SUCCESS);
     } else {
       System.out.println(FAIL);
     }
   }
 
-  public void send(int senderId, int receiverId) {
+  public boolean hasBalance(int id, int amount) {
+    int avail = getBalance(id);
+    if (avail >= amount) {
+      return true;
+    }
+    return false;
+  }
 
-    if (idBalanceMap.containsKey(senderId) && idBalanceMap.containsKey(receiverId)) {
+  public void send(int senderid, int receiverid) {
+
+    if (idBalanceMap.containsKey(senderid) && idBalanceMap.containsKey(receiverid)) {
       System.out.println(SUCCESS);
+      System.out.println("\nEnter the amount you want to send:");
+      int amount = sc.nextInt();
+      if (hasBalance(senderid, amount)) {
+        int senderBalance = getBalance(senderid);
+        String senderName = getName(senderid);
+        senderBalance = senderBalance - amount;
+        updateDetails(senderName, senderid, senderBalance);
+        int receiverBalance = getBalance(receiverid);
+        String receiverName = getName(receiverid);
+        receiverBalance = receiverBalance + amount;
+        updateDetails(receiverName, receiverid, receiverBalance);
+        System.out.println(SUCCESS);
+
+      } else {
+        System.out.println(FAIL);
+      }
+
     } else {
       System.out.println(FAIL);
     }
@@ -74,21 +143,28 @@ class Account extends Services {
   }
 
   public void updateDetails() {
+    LinkedHashMap<String, Integer> nameBalanceMap = new LinkedHashMap<>();
     nameBalanceMap.put(this.name, this.balance);
-    idBalanceMap.put(this.id, this.balance);
+    idBalanceMap.put(this.id, nameBalanceMap);
+  }
+
+  public void display(int id, String name, int balance) {
+    System.out.println("The Account Holder's name : " + name);
+    System.out.println("The Account Holder's id : " + id);
+    System.out.println("The Account Holder's balance: " + balance);
   }
 
   public void displayDetails() {
     System.out.println();
-    System.out.println("The Account Holder's name : " + this.name);
-    System.out.println("The Account Holder's id : " + this.id);
-    System.out.println("The Account Holder's balance: " + this.balance);
+    display(this.id, this.name, this.balance);
   }
 
   public void displayDetails(int id) {
     if (idBalanceMap.containsKey(id)) {
       System.out.println(SUCCESS);
-      displayDetails();
+      int balance = getBalance(id);
+      String name = getName(id);
+      display(id, name, balance);
     } else {
       System.out.println(FAIL);
     }
@@ -97,22 +173,21 @@ class Account extends Services {
 }
 
 public class App {
+  Scanner sc = new Scanner(System.in);
 
   public int reEnterId() {
-    System.out.println("\nRe enter your id to confirm: ");
-    Scanner sc = new Scanner(System.in);
+    System.out.println("\nEnter account id for verification and authentication: ");
     int id = sc.nextInt();
     return id;
   }
 
   public void sessionStart() {
-    Scanner sc = new Scanner(System.in);
     System.out.println("Enter your name to open account: ");
     String name = sc.nextLine();
     System.out.println("Enter the balance you want to deposit: ");
     int balance = sc.nextInt();
     Account account = new Account(name, balance);
-    System.out.println("Do you want to go to Account Services ?");
+    System.out.println("Do you want to go to Banking Services ?");
     System.out.println("Press 1 to Continue or 2 to Exit!");
     int choice = sc.nextInt();
     outer: if (choice == 1) {
